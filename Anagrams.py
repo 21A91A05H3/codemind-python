16ce11120c6a28b998ef1e7349cc80a57fbad253
21A91A05H3 <97424746+21A91A05H3@users.noreply.github.com>
2022-12-17 06:35:19 +0530

    s1=input() s2=input() for i in s1:     if i in s2:         print("True")         break     else:         print("False")         break

--- Anagrams.py
+++ Anagrams.py
@@ -1,13 +1,9 @@
-def ana(s,s1):
-    for char in s:
-        if char not in s1:
-            return False
-    return True
-s=input()
 s1=input()
-s=s.lower()
-s1=s1.lower()
-if(ana(s,s1)==True):
-    print('True')
-else:
-    print('False')
+s2=input()
+for i in s1:
+    if i in s2:
+        print("True")
+        break
+    else:
+        print("False")
+        break
